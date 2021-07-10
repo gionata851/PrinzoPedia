d9891c41e580df8aaa49e60a546f2665e785d79a
gionata851 <gionata.prinzo@gmail.com>
2021-07-10 17:29:00 +0200

    final commit

---
+++ PrinzoPediaEngine.js
@@ -0,0 +1,133 @@
+/*dichiarazione variabili scope globale*/
+
+const serverurl = 'https://api.dictionaryapi.dev/api/v2/entries/';
+var lang = '';
+var vocabolo = '';
+var definitionObject;
+var mainsection;
+
+window.onload = function()
+			{
+				document.getElementById('vocabolo').addEventListener("keypress", function(event) { enter(event); }, false);
+			}
+            
+
+/* funzione di ricerca */
+function ricerca(){
+    mainsection = document.querySelector('main');
+    mainsection.innerHTML = '';
+    lang = document.spedizione_vocabolo.lang.value;
+    vocabolo = document.spedizione_vocabolo.vocabolo.value.toLowerCase().replace(/ /g, '');
+    // console.log(vocabolo);
+    var ricerca = new Promise ((resolve,reject) => {
+        //let risposta;
+        let req = new XMLHttpRequest();
+        req.open('GET',`${serverurl}${lang}/${vocabolo}`);
+        req.send();
+        req.onreadystatechange = () => {
+            setTimeout( () => {
+                if (req.readyState == 4 && req.status == 200){
+                    definitionObject = JSON.parse(req.responseText);
+                    resolve(definitionObject);
+                    console.log("successfully resolved");
+                } else if (req.readyState == 4 && req.status == 404){
+                    definitionObject = JSON.parse(req.responseText);
+                    console.log("error 404");
+                    reject(definitionObject);
+                } else {
+                    definitionObject = JSON.parse(req.responseText);
+                    console.log("generic error");
+                    reject(definitionObject);
+                }
+            },50);
+        }
+        
+    })
+    ricerca
+        .then( (unfilteredDefinitionObject) => {
+            definitionObject = unfilteredDefinitionObject.filter((obj) => { return (obj.meanings != false) });
+
+            let wordsfound = document.createElement('div');
+            wordsfound.setAttribute('class','wordsfound');
+            if(definitionObject.length > 1)
+                wordsfound.innerText = `${definitionObject.length} words found`;
+            mainsection.appendChild(wordsfound);
+            
+            definitionObject.map((wordfound) =>{
+                let wordcontainer = document.createElement('div');
+                wordcontainer.setAttribute('class','wordcontainer');
+                mainsection.appendChild(wordcontainer);
+
+                let word = document.createElement('div');
+                word.setAttribute('class','wordhead word');
+                word.innerText = wordfound.word;
+                wordcontainer.appendChild(word);
+
+                if(wordfound.phonetics[0].text && wordfound.phonetics[0].text != false){
+
+                    let phonetics = document.createElement('div');
+                    phonetics.setAttribute('class','wordhead phonetics');
+                    phonetics.innerText = wordfound.phonetics[0].text || '' + wordfound.phonetics[1].text || '' + wordfound.phonetics[2].text || ''
+                    wordcontainer.appendChild(phonetics);
+                }
+
+                wordfound.meanings.map((meaning) => {
+                    let partofspeech = document.createElement('div');
+                    partofspeech.setAttribute('class','meaninghead pos');
+                    partofspeech.innerText = meaning.partOfSpeech;
+                    wordcontainer.appendChild(partofspeech);
+                    meaning.definitions.map((definition) =>{
+                        let def = document.createElement('div');
+                        def.setAttribute('class','wordbody def');
+                        def.innerText = definition.definition;
+                        wordcontainer.appendChild(def);
+                        
+                        if(definition.synonyms && definition.synonyms != false){
+                            let syn = document.createElement('div');
+                            syn.setAttribute('class','wordbody syn');
+                            syn.innerText ='Synonyms: ' +  definition.synonyms;
+                            wordcontainer.appendChild(syn);
+                        }
+                        if(definition.example){
+                            let example = document.createElement('div');
+                            example.setAttribute('class','wordbody example');
+                            example.innerText ='Example: ' +  definition.example;
+                            wordcontainer.appendChild(example);
+                        }
+                    })
+                })
+
+
+            })
+
+
+
+        })
+        .catch( (definitionObject) => {
+            mainsection = document.querySelector('main');
+            mainsection.innerHTML = '';
+            let wordsfound = document.createElement('div');
+            wordsfound.setAttribute('class','word');
+            wordsfound.innerHTML = definitionObject.title;
+            mainsection.appendChild(wordsfound);
+
+            let message = document.createElement('div');
+            message.setAttribute('class','syn');
+            message.innerHTML = definitionObject.message;
+            mainsection.appendChild(message);
+
+            let resolution = document.createElement('div');
+            resolution.setAttribute('class','pos');
+            resolution.innerHTML = definitionObject.resolution;
+            mainsection.appendChild(resolution);
+            // document.querySelectorAll('.def')[0].innerHTML = `${definitionObject.message} <br>
+            // ${definitionObject.resolution}`;
+        })
+}
+
+function enter(event){
+    if(event.keyCode === 13) {
+        event.preventDefault()
+        ricerca();
+    }
+};
---
+++ readme.txt
@@ -0,0 +1,10 @@
+My name is Gionata Prinzo, engineer and student at Eduonix.
+This is the project of the first section of the Javascript e-degree. With it I simply tried to use the dictionary idea shown by professor Gray Lewis and to go further, showing the most amount of information I could. This means that the app will publish all the words, all the meanings and definitions given by the api, showing as well examples, phonetics and synonyms if provided.
+As you may see, you can choose the language as well. I focused on languages I know, more or less, mostly for debugging purposes.
+Fortunately I already know HTML and CSS so wasn't a problem for me to structure and visually organize the data.
+
+I debugged a lot and it seems working fine. I admit I'm proud of it! I hope you appreciate as well.
+
+Regards,
+
+Gionata Prinzo
